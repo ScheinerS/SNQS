9074caf28c8d156f12b51c0414fd0a44d07968d5
Santiago <santiago.scheiner@lip6.fr>
2023-06-12 18:05:40 +0200

    energy consumption

---
+++ energy_consumption.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 16:48:21 2023
+
+@author: santiago
+"""
+
+# Based on Pascal's talk. To be implemented in the future for simulating the energy consumption of different protocols.
