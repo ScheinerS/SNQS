32ef7a48c368c2045b974b56d500291f40b1bb15
Santiago <santiago.scheiner@lip6.fr>
2023-07-28 12:16:12 +0200

    plt_parameters

---
+++ plt_parameters.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 12:08:59 2023
+
+@author: santiago
+"""
+import matplotlib.pyplot as plt
+
+plt.close('all')
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.usetex'] = True
+
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 22}
+
+plt.rc('font', **font)
